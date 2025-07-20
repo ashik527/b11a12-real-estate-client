c07444e84c30ddaaca449c285e309a70edeca283
Ashik git project <ashikurrahmanshk@gmail.com>
2025-07-20 22:20:42 +0600

    client update 2

--- src/hooks/useAxiosSecure.js
+++ src/hooks/useAxiosSecure.js
@@ -1,21 +1,10 @@
 // src/hooks/useAxiosSecure.js
 import axios from 'axios';
-import { useEffect } from 'react';
 
 const useAxiosSecure = () => {
   const axiosSecure = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
   });
-
-  useEffect(() => {
-    axiosSecure.interceptors.request.use((config) => {
-      const token = localStorage.getItem('access-token');
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    });
-  }, [axiosSecure]);
 
   return axiosSecure;
 };
